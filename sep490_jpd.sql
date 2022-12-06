996394c4d5e700228935f8e834f1b86f6477680c
hungnqce150879 <hungnqce150879@fpt.edu.vn>
2022-12-06 18:08:11 +0700

    Update sep490_jpd.sql

--- sep490_jpd.sql
+++ sep490_jpd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2022 at 09:29 AM
+-- Generation Time: Dec 06, 2022 at 12:07 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -20,6 +20,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `sep490_jpd`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account_code`
+--
+
+CREATE TABLE `account_code` (
+  `codeID` int(11) NOT NULL,
+  `code` varchar(6) NOT NULL,
+  `expiryDate` datetime NOT NULL,
+  `userID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -94,7 +107,7 @@ INSERT INTO `forum_comment` (`comment_id`, `post_id`, `comment_content`, `userID
 (11, 6, 'Về cơ bản thì không cấm nhưng cũng loằng ngoằng nhỉ.', 2, '2022-12-06 14:01:29', '2022-12-06 14:01:29', 'active'),
 (12, 6, 'Được nha bác, chỉ cần trường mới của bác cấp cho 在学証明書 nộp cho cục là oki·', 3, '2022-12-06 14:01:51', '2022-12-06 14:01:51', 'disable'),
 (13, 8, 'Giám đốc công ty em, kể hồi trước ổng toàn làm rồi ngủ luôn ở bàn xong dậy làm tiếp, và xe cấp cứu đến liên tục bế ổng đi :v', 1, '2022-12-06 14:12:41', '2022-12-06 14:12:41', 'active'),
-(14, 8, 'Tăng ca cũng được nhưng phải có tiền tăng ca, chứ tăng ca mà \"cống hiến cho công việc, tiền không quan trọng\" thì thôi bỏ sớm =)))', 3, '2022-12-06 14:13:07', '2022-12-06 14:13:07', 'disable');
+(14, 8, 'Tăng ca cũng được nhưng phải có tiền tăng ca, chứ tăng ca mà \"cống hiến cho công việc, tiền không quan trọng\" thì thôi bỏ sớm =)))', 3, '2022-12-06 14:13:07', '2022-12-06 14:13:07', 'active');
 
 -- --------------------------------------------------------
 
@@ -121,11 +134,16 @@ INSERT INTO `forum_post` (`post_id`, `topic_id`, `post_title`, `post_content`, `
 (1, 1, 'Dịch viết', 'Đoạn này câu cuối nên dịch như thế nào đây ạ =(((<br><br>広島市の松井一美市長は今日の平和宣言で、核兵器は「絶対悪」と呼びかける。安倍首相もルース駐日米大使も肝に銘じてほしい。私たちも伝承者としての出発点に当たり、心に傷みつけたい。<br>', 1, '2022-12-06 13:48:24', '2022-12-06 13:48:24', 'active'),
 (2, 1, 'Chín', 'Từ chín trong Nấu chín đồ ăn hoặc đồ ăn này chưa chín thì dùng từ nào vậy ah?', 1, '2022-12-06 13:50:29', '2022-12-06 13:50:29', 'active'),
 (3, 2, 'Viết chữ', 'Mn có cách nào học viết nhanh ko ạ. Trc giờ e toàn nhìn mặt chữ r đọc chứ ít viết lắm. <br>Nên giờ muốn viết để sau thi kiếm ít cái chứng chỉ cho tiện mà giờ viết đi viết lại k hiệu quả cho lắm toàn thiếu nét<br>', 1, '2022-12-06 13:56:56', '2022-12-06 13:56:56', 'active'),
-(4, 2, '月末 hay 上旬', 'Cuối tháng thì dùng 月末 hay 上旬. 2 từ này khác nhau như nào ạ mn', 1, '2022-12-06 13:58:49', '2022-12-06 13:58:49', 'active'),
+(4, 2, '月末 hay 上旬', 'Cuối tháng thì dùng 月末 hay 上旬. 2 từ này khác nhau như nào ạ mn', 1, '2022-12-06 13:58:49', '2022-12-06 13:58:49', 'disable'),
 (5, 3, 'TRUNG TÂM ĐI NHẬT UY TÍN', 'Em đang muốn đi sang Nhật, mọi người cho em hỏi trước đó mọi người sang có thông qua trung tâm nào không ạ. <br>Review giúp em với ạ. Trung tâm nào uy tín ạ, chứ bạn em đi Hàn gặp trung tâm nó làm ăn hời hợt cả năm chưa bay được :((((<br>', 1, '2022-12-06 13:59:55', '2022-12-06 13:59:55', 'active'),
-(6, 3, 'chuyển trường senmon', 'mình đang học năm nhất senmon , mà giờ muốn học hết năm nhất rồi chuyển qua trường khác học , thì có được k nhỉ', 1, '2022-12-06 14:00:59', '2022-12-06 14:00:59', 'disable'),
+(6, 3, 'chuyển trường senmon', 'mình đang học năm nhất senmon , mà giờ muốn học hết năm nhất rồi chuyển qua trường khác học , thì có được k nhỉ', 1, '2022-12-06 14:00:59', '2022-12-06 14:00:59', 'active'),
 (7, 4, 'Văn hóa NB nói riêng và văn hóa tư bản nói chung!', 'Thấy bác ở dưới bảo Vin làm kiểu vắt kiệt sức theo đúng nghĩa đen và thành quả nhận lại thì cũng tương xứng.\r\nKhông nói điêu nhưng giai đoạn phát triển thần tốc của mấy thằng tư bản nó làm khiếp thật, làm ngày làm đêm làm đến điên đến dại nên nó mới có ngày hôm nay ấy chứ các bác nhỉ? Em thấy mấy bác làm nhiều thì sợ hãi các thứ nhưng mà có làm như vậy thì nó mới phát triển được, genz giờ không biết tăng ca cái có giảy nãy lên không hay vẫn mong muốn được bào sức lao động nhỉ? \r\nCàng hiện đại em', 2, '2022-12-06 08:06:55', '2022-12-06 08:06:55', 'active'),
-(8, 4, 'Văn hóa NB nói riêng và văn hóa tư bản nói chung!', 'Thấy bác ở dưới bảo Vin làm kiểu vắt kiệt sức theo đúng nghĩa đen và thành quả nhận lại thì cũng tương xứng.<br>Không nói điêu nhưng giai đoạn phát triển thần tốc của mấy thằng tư bản nó làm khiếp thật, làm ngày làm đêm làm đến điên đến dại nên nó mới có ngày hôm nay ấy chứ các bác nhỉ? Em thấy mấy bác làm nhiều thì sợ hãi các thứ nhưng mà có làm như vậy thì nó mới phát triển được, genz giờ không biết tăng ca cái có giảy nãy lên không hay vẫn mong muốn được bào sức lao động nhỉ? <br>', 2, '2022-12-06 14:12:10', '2022-12-06 14:12:10', 'disable');
+(8, 4, 'Văn hóa NB nói riêng và văn hóa tư bản nói chung!', 'Thấy bác ở dưới bảo Vin làm kiểu vắt kiệt sức theo đúng nghĩa đen và thành quả nhận lại thì cũng tương xứng.<br>Không nói điêu nhưng giai đoạn phát triển thần tốc của mấy thằng tư bản nó làm khiếp thật, làm ngày làm đêm làm đến điên đến dại nên nó mới có ngày hôm nay ấy chứ các bác nhỉ? Em thấy mấy bác làm nhiều thì sợ hãi các thứ nhưng mà có làm như vậy thì nó mới phát triển được, genz giờ không biết tăng ca cái có giảy nãy lên không hay vẫn mong muốn được bào sức lao động nhỉ? <br>', 2, '2022-12-06 14:12:10', '2022-12-06 14:12:10', 'active'),
+(9, 2, 'test edit 2', 'test edit 2', 1, '2022-12-06 16:44:06', '2022-12-06 16:47:31', 'active'),
+(10, 1, 'test 2', 'test 2', 1, '2022-12-06 16:50:19', '2022-12-06 16:50:19', 'active'),
+(11, 2, 'test 3', 'test 3', 1, '2022-12-06 16:50:31', '2022-12-06 16:50:31', 'active'),
+(12, 4, 'test 4', 'test 4', 1, '2022-12-06 16:50:43', '2022-12-06 16:50:43', 'active'),
+(13, 4, 'test 5', 'test 5', 1, '2022-12-06 16:50:58', '2022-12-06 16:50:58', 'active');
 
 -- --------------------------------------------------------
 
@@ -148,7 +166,7 @@ CREATE TABLE `forum_report_comment` (
 
 INSERT INTO `forum_report_comment` (`report_comment_id`, `user_report_id`, `report_comment_reason`, `comment_id`, `report_comment_date`, `status`) VALUES
 (3, 2, 'Bình luận lạc đề', 8, '2022-12-06 14:24:44', 'active'),
-(4, 2, 'Bình luận vi phạm', 10, '2022-12-06 14:25:06', 'active');
+(4, 2, 'Bình luận vi phạm', 10, '2022-12-06 14:25:06', 'disable');
 
 -- --------------------------------------------------------
 
@@ -188,7 +206,8 @@ INSERT INTO `forum_report_notification` (`report_notification_id`, `userID`, `re
 (15, 2, 'Lý Do Xoá: Bài đăng không hợp lý', 8, NULL, 'not read', 'active'),
 (16, 1, 'Lý Do Xoá: Nội dung vi phạm', 6, NULL, 'read', 'active'),
 (17, 3, 'Lý Do Xoá: Bình luận không phù hợp', NULL, 14, 'not read', 'active'),
-(18, 3, 'Lý Do Xoá: Bình luận vi phạm nguyên tắc', NULL, 12, 'not read', 'active');
+(18, 3, 'Lý Do Xoá: Bình luận vi phạm nguyên tắc', NULL, 12, 'not read', 'active'),
+(19, 1, 'Lý Do Xoá: 1234', 4, NULL, 'not read', 'active');
 
 -- --------------------------------------------------------
 
@@ -210,8 +229,7 @@ CREATE TABLE `forum_report_post` (
 --
 
 INSERT INTO `forum_report_post` (`report_post_id`, `user_report_id`, `report_post_reason`, `post_id`, `report_post_date`, `status`) VALUES
-(3, 2, 'Nội dung không phù hợp', 5, '2022-12-06 14:24:18', 'active'),
-(4, 2, 'Nội dung vi phạm', 4, '2022-12-06 14:24:31', 'active');
+(3, 2, 'Nội dung không phù hợp', 5, '2022-12-06 14:24:18', 'active');
 
 -- --------------------------------------------------------
 
@@ -230,11 +248,15 @@ CREATE TABLE `forum_topic` (
 --
 
 INSERT INTO `forum_topic` (`topic_id`, `topic_name`, `topic_status`) VALUES
-(1, 'Dịch', 'active'),
-(2, 'Học Tiếng Nhật', 'active'),
+(1, 'Dịch 3', 'active'),
+(2, 'Học Tiếng Nhật 3', 'active'),
 (3, 'Du Học Nhật Bản', 'active'),
 (4, 'Văn Hoá Nhật Bản', 'active'),
-(5, 'Góc Chia Sẻ', 'disable');
+(5, 'Góc Chia Sẻ', 'active'),
+(6, 'test', 'active'),
+(7, 'tâm topic', 'active'),
+(8, 'test 2', 'active'),
+(9, 'test 3', 'active');
 
 -- --------------------------------------------------------
 
@@ -247,6 +269,18 @@ CREATE TABLE `grammar` (
   `levelID` int(11) NOT NULL,
   `structure` varchar(255) NOT NULL,
   `use` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grammarhistory`
+--
+
+CREATE TABLE `grammarhistory` (
+  `grammarID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `grammarHistoryStatus` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -320,6 +354,13 @@ CREATE TABLE `notification` (
   `status` tinyint(1) NOT NULL DEFAULT 0,
   `userID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `notification`
+--
+
+INSERT INTO `notification` (`notificationID`, `title`, `details`, `time`, `status`, `userID`) VALUES
+(1, '123', '123', '2022-12-06 17:00:03', 0, 3);
 
 -- --------------------------------------------------------
 
@@ -425,6 +466,14 @@ CREATE TABLE `type` (
   `type` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` (`type`) VALUES
+('Hiragana'),
+('Katakana');
+
 -- --------------------------------------------------------
 
 --
@@ -449,11 +498,19 @@ INSERT INTO `user` (`userID`, `email`, `password`, `username`, `phone`, `avatar`
 (1, 'user@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 'Người Dùng', '', 'images\\user.png', 'Người dùng'),
 (2, 'cm@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 'Content Manager', '', 'images\\user.png', 'Quản lí nội dung'),
 (3, 'admin@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 'Admin', '', 'images\\user.png', 'Quản trị viên'),
-(4, 'sang@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 'saing', '0826278177', 'images\\user.png', 'Người dùng');
+(4, 'sang@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 'saing', '0826278177', 'images\\user.png', 'Người dùng'),
+(5, 'user@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'HungNQCE150879', '0349554811', 'images\\user.png', 'Người dùng');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `account_code`
+--
+ALTER TABLE `account_code`
+  ADD PRIMARY KEY (`codeID`),
+  ADD KEY `userID` (`userID`);
 
 --
 -- Indexes for table `alphabet`
@@ -529,6 +586,13 @@ ALTER TABLE `forum_topic`
 ALTER TABLE `grammar`
   ADD PRIMARY KEY (`grammarID`),
   ADD KEY `levelID` (`levelID`);
+
+--
+-- Indexes for table `grammarhistory`
+--
+ALTER TABLE `grammarhistory`
+  ADD PRIMARY KEY (`grammarID`,`UserID`),
+  ADD KEY `UserID` (`UserID`);
 
 --
 -- Indexes for table `kanji`
@@ -619,6 +683,12 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `account_code`
+--
+ALTER TABLE `account_code`
+  MODIFY `codeID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `alphabet`
 --
 ALTER TABLE `alphabet`
@@ -628,7 +698,7 @@ ALTER TABLE `alphabet`
 -- AUTO_INCREMENT for table `answer`
 --
 ALTER TABLE `answer`
-  MODIFY `AnswerID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `AnswerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `examplegrammar`
@@ -646,7 +716,7 @@ ALTER TABLE `forum_comment`
 -- AUTO_INCREMENT for table `forum_post`
 --
 ALTER TABLE `forum_post`
-  MODIFY `post_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `post_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `forum_report_comment`
@@ -658,7 +728,7 @@ ALTER TABLE `forum_report_comment`
 -- AUTO_INCREMENT for table `forum_report_notification`
 --
 ALTER TABLE `forum_report_notification`
-  MODIFY `report_notification_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `report_notification_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `forum_report_post`
@@ -670,7 +740,7 @@ ALTER TABLE `forum_report_post`
 -- AUTO_INCREMENT for table `forum_topic`
 --
 ALTER TABLE `forum_topic`
-  MODIFY `topic_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `topic_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `grammar`
@@ -700,13 +770,13 @@ ALTER TABLE `message`
 -- AUTO_INCREMENT for table `notification`
 --
 ALTER TABLE `notification`
-  MODIFY `notificationID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `notificationID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `QuestionID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `QuestionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `result`
@@ -742,7 +812,7 @@ ALTER TABLE `test`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -807,6 +877,13 @@ ALTER TABLE `forum_report_post`
 --
 ALTER TABLE `grammar`
   ADD CONSTRAINT `grammar_ibfk_1` FOREIGN KEY (`levelID`) REFERENCES `level` (`levelID`);
+
+--
+-- Constraints for table `grammarhistory`
+--
+ALTER TABLE `grammarhistory`
+  ADD CONSTRAINT `grammarhistory_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `grammarhistory_ibfk_2` FOREIGN KEY (`grammarID`) REFERENCES `grammar` (`grammarID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `kanji`
